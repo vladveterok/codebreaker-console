4e13177ad7430119cf8be0a2674234eb7814c26c
vladveterok <vl.berezhnoi@gmail.com>
2020-07-05 00:55:27 +0300

    Removed all

--- spec/console/console_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Console do
-  subject(:console) { described_class.new }
-
-  context 'when initializing' do
-    it { expect(console.state).to be_a(GameMenuState) }
-  end
-end
