651b13383972ac22f0b471bd1106942c6da7526a
vladveterok <vl.berezhnoi@gmail.com>
2020-07-06 15:27:48 +0300

    Start game from index.rb

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-# game paths
-
-# console paths
+require 'bundler/setup'
 require 'codebreaker'
+
 require 'i18n'
+
 require_relative 'config/application'
 require_relative 'console/modules/statistics'
+require_relative 'console/console'
 require_relative 'console/states/console_state'
 require_relative 'console/states/game_menu_state'
 require_relative 'console/states/game_registration_state'
